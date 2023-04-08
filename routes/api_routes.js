42c0599af816436f244dd3f689cc0553fa78ae6a
Austin Lewis <austinlewis165@gmail.com>
2023-04-07 23:36:46 -0400

    added uuid for id generation. finally got get/post to work for saving notes. tackling delete now

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,9 +1,29 @@
 const path = require('path');
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const fs = require ('fs');
 
+//***THE FOLLOWING API ROUTES SHOULD BE CREATED */
+//**"GET /api/notes" should read the "db.json" file and return all saved notes as JSON */
+//**"POST /api/notes" should receive a new note to save on the request body, add it to the "db.json" file, and then return the new note to the client. I'll need to find a way to give each note a unique id when it's saved (:id) */
+// app.post('/api/notes' )
 
-// router.get('/notes', (req, res) => {
-//     res.sendFile(path.join(process.cwd(), 'views/notes.html'))
-// })
+//***needed to be async to allow 'await' to work */
+router.get('/api/notes', async (req, res) => {
+    const dbData = await JSON.parse(fs.readFileSync('db/db.json', 'utf-8'));
+    res.json(dbData);
+})
+
+router.post('/api/notes', (req, res) => {
+    const dbData = JSON.parse(fs.readFileSync('db/db.json', 'utf-8'));
+    const newData = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4()
+    };
+    dbData.push(newData);
+    fs.writeFileSync('db/db.json', JSON.stringify(dbData, null, 2) + '\n');
+    res.json(dbData)
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
-
-const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 const app = express();
-
-const html_routes = require('./routes/html_routes');
 const api_routes = require('./routes/api_routes');
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static('public'))
+
 //***HTML ROUTES */
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
@@ -17,16 +16,7 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 })
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-})
-
-
-
-//***THE FOLLOWING API ROUTES SHOULD BE CREATED */
-//**"GET /api/notes" should read the "db.json" file and return all saved notes as JSON */
-//**"POST /api/notes" should receive a new note to save on the request body, add it to the "db.json" file, and then return the new note to the client. I'll need to find a way to give each note a unique id when it's saved (:id) */
-// app.post('/api/notes' )
-
+//***API ROUTES */
+app.use(api_routes);
 
 app.listen(PORT, () => console.log('Listening on port %s', PORT));
